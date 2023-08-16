0309c7f339b9a0bb15184cabeb548e41edf123d9
InBeomJin <ben9269960@gmail.com>
2023-08-16 16:39:35 +0900

    hello world

--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,26 @@
-class Strong {
-
-  final double strengthLevel = 15000;
-}
-
-class QuickRunner{
-  void runQuick(){
-    print('ruuuuuun');
-  }
-}
-
-
-class Tall {
-  final double height = 1.99;
-}
-
-abstract class Human {
-  final String name;
-
-  Human({required this.name});
-  void sayHello(){
-    print("HI my name is $name");
-  }
-}
-
-class Player extends Human with Strong, QuickRunner, Tall {
-  final Team team;
-
-  Player({
-    required this.team,
-    required String name
-}) : super(name: name);
-
-  @override
-  void sayHello(){
-    super.sayHello();
-    print("and I p;ay for ${team}");
-  }
-
-}
-
-class Horse with Strong, QuickRunner{}
-
-class kid with QuickRunner{}
-
-enum Team { red, blue }
-enum XPLevel { beginner,medium, pro}
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 void main(){
-  var player = Player(team: Team.red, name: 'blue');
+  runApp( App() );
+}
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar( //named parametes로 이해
+          title: Text('Hello flutter!'),
+          backgroundColor: Colors.brown,
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello world'),
+
+        )
+      ),
+    );
+  }
 
 }
